d64a79b733d121c495823b4652723a0a8c4c1bac
AshuSharma16 <ashusharma_16@hotmail.com>
2023-02-20 14:07:29 +0530

    first commit

---
+++ Basic-example/src/test/java/com/ashu16/rest/webservice/Basicexample/BasicExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashu16.rest.webservice.Basicexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
