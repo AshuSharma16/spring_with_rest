063bfd78bedc3818af283f6cc6ce908c91fae881
AshuSharma16 <ashusharma_16@hotmail.com>
2023-02-20 14:16:48 +0530

    removed duplicate data using distinct

---
+++ Basic-example/src/main/java/com/ashu16/rest/webservice/Basicexample/RemoveDuplicateDataController.java
@@ -0,0 +1,25 @@
+package com.ashu16.rest.webservice.Basicexample;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RemoveDuplicateDataController {
+
+	@GetMapping("/getuniquedata")
+	public List<String> getUniqueData(){
+		return removeDuplicateData();
+	}
+
+	private List<String> removeDuplicateData() {
+		List<String> names = Arrays.asList("Karan", "shyam", "Shwetha", "Sandhya", "manmohan", "sandhya", "SANDHYA", "KARAN");
+		
+		List<String> uniqueData = names.stream().map(name->name.toLowerCase()).distinct().collect(Collectors.toList());
+		return uniqueData;
+		
+	}
+}
