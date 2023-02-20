b4f0822414128f5fc679fc88135f24d59445b3c8
AshuSharma16 <ashusharma_16@hotmail.com>
2023-02-20 15:10:26 +0530

    refector package name

--- Basic-example/src/main/java/com/ashu16/rest/webservice/Basicexample/controller/RemoveDuplicateDataController.java
+++ Basic-example/src/main/java/com/ashu16/rest/webservice/Basicexample/controller/RemoveDuplicateDataController.java
@@ -1,4 +1,4 @@
-package com.ashu16.rest.webservice.Basicexample;
+package com.ashu16.rest.webservice.Basicexample.controller;
 
 import java.util.Arrays;
 import java.util.List;
@@ -10,14 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RemoveDuplicateDataController {
 
-	@GetMapping("/getuniquedata")
+	@GetMapping(path = "/getuniquedata")
 	public List<String> getUniqueData(){
 		return removeDuplicateData();
 	}
 
 	private List<String> removeDuplicateData() {
 		List<String> names = Arrays.asList("Karan", "shyam", "Shwetha", "Sandhya", "manmohan", "sandhya", "SANDHYA", "KARAN");
-		
 		List<String> uniqueData = names.stream().map(name->name.toLowerCase()).distinct().collect(Collectors.toList());
 		return uniqueData;
 		
